65d7f3abe103ca8f3c340d073b5eca9a85514cdd
sbnn3 <sebastian.neagu@outlook.com>
2022-11-29 15:58:30 +0000

    Customisez admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Course, Category
 
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category', 
+        'price',
+        'image',
+    )
 
-admin.site.register(Course)
-admin.site.register(Category)
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Category, CategoryAdmin)
