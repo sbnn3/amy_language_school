10c40f2511135ea90235ea88728ba23bbd921383
sbnn3 <sebastian.neagu@outlook.com>
2022-11-30 14:53:09 +0000

    Added category filtering

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Course
+from .models import Course, Category
 
 
 def all_courses(request):
@@ -9,8 +9,14 @@ def all_courses(request):
 
     courses = Course.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            courses = courses.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +29,7 @@ def all_courses(request):
     context = {
         'courses': courses,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'courses/courses.html', context)
