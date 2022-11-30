3f7a34e88506552f9a68666ea700fcfba8fe0c7f
sbnn3 <sebastian.neagu@outlook.com>
2022-11-30 13:31:48 +0000

    Added search functionality on site

--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Course
 
 
@@ -6,9 +8,21 @@ def all_courses(request):
     """ A view to show all courses available, including sorting and searching queries """
 
     courses = Course.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('courses'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            courses = courses.filter(queries)
 
     context = {
         'courses': courses,
+        'search_term': query,
     }
 
     return render(request, 'courses/courses.html', context)
