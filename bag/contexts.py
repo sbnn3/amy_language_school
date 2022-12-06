6be0e09a0862fc6824af6904a6ed65129129585c
sbnn3 <sebastian.neagu@outlook.com>
2022-12-06 20:38:27 +0000

    Updated context processor

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
-
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 
 def bag_contents(request):
@@ -8,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     course_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        course = get_object_or_404(Course, pk=item_id)
+        total += quantity * course.price
+        course_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'course': course,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
