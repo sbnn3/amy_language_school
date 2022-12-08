7c92a36e39bb53e2451b52349f60a83a74bb832a
sbnn3 <sebastian.neagu@outlook.com>
2022-12-08 19:24:22 +0000

    Added notification css, shopping bag message alerts update and other aditional messages

--- bag/views.py
+++ bag/views.py
@@ -12,13 +12,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    course = Course.objects.get(pk=item_id)
+    course = get_object_or_404(Course, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {course.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {course.name} to your bag')
@@ -29,15 +30,18 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """ Adjust the quantity of the specified course to the specified amount """
 
+    course = get_object_or_404(Course, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {course.name} quantity to {bag[item_id]}')
     else:
         del bag[item_id]
         if not bag[item_id]:
             bag.pop(item_id)
+            messages.success(request, f'Removed {course.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -45,13 +49,16 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """ Remove the item from the shopping bag """
 
-    try:    
+    try: 
+        course = get_object_or_404(Course, pk=item_id)
         bag = request.session.get('bag', {})
         bag.pop(item_id)
+        messages.success(request, f'Removed {course.name} from your bag')
 
         request.session['bag'] = bag
 
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
