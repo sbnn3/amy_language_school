b19b5740486e0584854ff078665796abf58f92aa
sbnn3 <sebastian.neagu@outlook.com>
2022-12-11 17:27:44 +0000

    Completed webhook handler and solved the stripe succeeded issue in webhook handler file using the source from code institute and updated the handle payment intent succeeded

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -59,7 +59,7 @@ form.addEventListener('submit', function(ev) {
     var postData = {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
-        'save-info': saveInfo,
+        'save_info': saveInfo,
     };
     var url = '/checkout/cache_checkout_data/';
 
@@ -111,7 +111,8 @@ form.addEventListener('submit', function(ev) {
                 }
             }
         });
-    }).fail(function() {
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
         location.reload();
     })
 });
--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,7 @@ import json
 @require_POST
 def cache_checkout_data(request):
     try:
+        print(request.POST)
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
@@ -47,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     course = Course.objects.get(id=item_id)
