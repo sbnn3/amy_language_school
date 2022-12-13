f36a4c5829c510ee944fbffe2d75c3c6553ea89b
sbnn3 <sebastian.neagu@outlook.com>
2022-12-13 18:52:07 +0000

    Added course view, url and created temaplte for adding course option - Course Admin

--- courses/views.py
+++ courses/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Course, Category
+from .forms import CourseForm
 
 
 def all_courses(request):
@@ -64,3 +66,16 @@ def course_detail(request, course_id):
     }
 
     return render(request, 'courses/course_detail.html', context)
+
+
+def add_course(request):
+    """
+    Add a course to the store
+    """
+    form = CourseForm()
+    template = 'courses/add_course.html'
+    context = {
+        'form': form,
+    }
+    
+    return render(request, template, context)
