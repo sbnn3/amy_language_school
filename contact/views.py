34cf98167a2a02de2e7fa9fcba540559d5df7988
sbnn3 <sebastian.neagu@outlook.com>
2022-12-15 19:52:02 +0000

    Defined message in views for contact page

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .forms import MessageForm
+from django.contrib import messages
 
-# Create your views here.
+
+def message(request):
+    """
+    Saves the contact form to database
+    """
+    form = MessageForm()
+    if request.method == 'POST' and request.user.is_authenticated:
+        form = MessageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your message has been sent to Admin!')
+            
+            return render(request, "home/index.html")
+        else:
+            messages.error(request, 'Failed to send a message. Please, make sure the form is valid!')
+    else:
+        form = MessageForm()
+        context = {
+            'form': form,
+        }
+        return render(request, 'contact/contact.html', context)
+    return render(request, 'contact/contact.html')
