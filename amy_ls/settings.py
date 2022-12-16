bf0f4b920194b7e1c1387fcac9c5068ca60f15e8
sbnn3 <sebastian.neagu@outlook.com>
2022-12-16 10:50:51 +0000

    Updated link for opened book image at topnav bar and favicon

--- amy_ls/settings.py
+++ amy_ls/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = 'django-insecure-m^(3e)0joe2ksjxs^##_jpx*w!@*tw^=d9m9o1p796bt0adp8&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['amy-language-school.herokuapp.com', 'localhost']
 
