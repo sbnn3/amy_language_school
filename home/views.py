a44ed1ed71bd4f854a9bfda2297335dca06c14dd
sbnn3 <sebastian.neagu@outlook.com>
2022-12-16 16:35:09 +0000

    Updated home views and settings in order to make a proper responsive design

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
 
-
+@xframe_options_exempt
 def index(request):
     """ A view to return the index page """
 
